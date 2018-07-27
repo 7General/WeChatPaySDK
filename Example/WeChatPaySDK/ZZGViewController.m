ec0ea8b712d8c05ab1b71931f9e1cc55c5441f01
wanghuizhou <wanghuizhou@guazi.com>
2018-07-27 16:51:29 +0800

    添加微信分享 和登录

--- Example/WeChatPaySDK/ZZGViewController.m
+++ Example/WeChatPaySDK/ZZGViewController.m
@@ -21,7 +21,8 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [MSWechatPayHelper WeChatPayTest];
+    
+//    [MSWechatPayHelper WeChatPayTest];
     /*
     <xml><app><![CDATA[
      {"appId":"wx16e73c94cb7048ac",
@@ -44,6 +45,41 @@
 //    request.timeStamp = @"1531881641";
 //    request.sign = @"B04B36F82A02BEBE49C0BA84D1844E80";
 //    [MSWechatPayHelper WakeupWeChatPay:request];
+//    [self shareweChat];
+    [self weChatLogin];
+}
+
+- (void)shareweChat {
+    //创建发送对象实例
+    SendMessageToWXReq *sendReq = [[SendMessageToWXReq alloc] init];
+    sendReq.bText = NO;//不使用文本信息
+    sendReq.scene = 1;//0 = 好友列表 1 = 朋友圈 2 = 收藏
+    
+    //创建分享内容对象
+    WXMediaMessage *urlMessage = [WXMediaMessage message];
+    urlMessage.title = @"分享标题";
+    urlMessage.description = @"分享描述";//
+    [urlMessage setThumbImage:[UIImage imageNamed:@"123.jpg"]];
+    
+    //创建多媒体对象
+    WXWebpageObject *webObj = [WXWebpageObject object];
+    webObj.webpageUrl = @"http://www.osjoin.com";//分享链接
+    
+    //完成发送对象实例
+    urlMessage.mediaObject = webObj;
+    sendReq.message = urlMessage;
+    
+    //发送分享信息
+    [WXApi sendReq:sendReq];
+}
+
+- (void)weChatLogin {
+    //构造SendAuthReq结构体
+    SendAuthReq* req =[[SendAuthReq alloc] init];
+    req.scope = @"snsapi_userinfo" ;
+    req.state = @"123" ;
+    //第三方向微信终端发送一个SendAuthReq消息结构
+    [WXApi sendReq:req];
 }
 
 @end
